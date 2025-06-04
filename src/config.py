aef31ab4f421089cc308312c40aa48035b7ca8fa
Seinarukiro <seinarukiro@gmail.com>
2025-06-04 18:51:55 +0200

    init: Channel events handler

--- src/config.py
+++ src/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    API_ID = int(os.getenv('API_ID', '7248451'))
-    API_HASH = os.getenv('API_HASH', 'db9b16eff233ee8dfd7c218138cb2e10')
-    BOT_TOKEN = os.getenv('BOT_TOKEN', '7944326047:AAHHD8lQ-weNEtylINDvxZTjLGUZ04VtN2c')
+    API_ID = int(os.getenv('API_ID'))
+    API_HASH = os.getenv('API_HASH')
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
     REDIS_HOST = os.getenv('REDIS_HOST', 'redis')
     REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
     REDIS_DB = int(os.getenv('REDIS_DB', 0))
